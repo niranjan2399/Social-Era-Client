8b0dec25a21e232e8be39cac9cdfc23dfd372c7b
Niranjan Kumar <niranjanpid23@gmail.com>
2021-07-03 13:35:17 +0530

    navbar completed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./navBar.scss";
 import SearchBar from "../search/SearchBar";
-import { AccountCircle } from "@material-ui/icons";
+import { AccountCircle, Person } from "@material-ui/icons";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="left">
         <div className="hamburger">
-          <input type="checkbox" class="menu-btn" id="menu-btn" />
-          <label for="menu-btn" class="menu-icon">
-            <span class="navicon"></span>
+          <input type="checkbox" className="menu-btn" id="menu-btn" />
+          <label htmlFor="menu-btn" className="menu-icon">
+            <span className="navicon"></span>
           </label>
         </div>
         <Link to="/" className="logo">
@@ -21,9 +22,23 @@ function Navbar() {
           </span>
         </Link>
       </div>
+      <SearchBar />
       <div className="right">
-        <SearchBar />
-        <AccountCircle className="account" />
+        <div className="account_div">
+          <AccountCircle className="account" />
+          <div className="username">niranjan</div>
+        </div>
+        <div className="person_div">
+          <Person className="person" />
+          <div>1</div>
+        </div>
+        <div className="notification_div">
+          <NotificationsIcon className="noti" />
+          <div>2</div>
+        </div>
+        <div className="more">
+          <i className="lzf7d6o1"></i>
+        </div>
       </div>
     </div>
   );
--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Search } from '@material-ui/icons'
-import './search.scss'
+import React from "react";
+import { Search } from "@material-ui/icons";
+import "./search.scss";
 
 function SearchBar() {
-	return (
-		<div className='search'>
-			<Search className='searchIcon' />
-			<input type="text" name="" id="" placeholder='Search for a friend, post or video' />
-		</div>
-	)
+  return (
+    <div className="search">
+      <div className="searchIconDiv">
+        <Search className="searchIcon" />
+      </div>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Search for a friend, post or video"
+      />
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
