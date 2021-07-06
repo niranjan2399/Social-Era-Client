7c26c7a1f77fc8e81e3353eb65533b2f8cf7f83e
Niranjan Kumar <niranjanpid23@gmail.com>
2021-07-06 21:52:18 +0530

    homepage completely responsive

--- src/components/leftSection/LeftSection.js
+++ src/components/leftSection/LeftSection.js
@@ -4,48 +4,50 @@ import data from "./toRender";
 
 function LeftSection() {
   return (
-    <div className='left_section'>
+    <div className="left_section">
       {data.map((list) => {
         return (
-          <div className='tag'>
+          <div className="tag">
             <list.logo />
             <span>{list.text}</span>
           </div>
         );
       })}
       <hr />
-      <span className='friend_count'>Friends(0)</span>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
-      </div>
-      <div className="friends">
-        <div className="pic"></div>
-        <span className="name">Siddha</span>
+      <div className="friends_div">
+        <span className="friend_count">Friends(0)</span>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
+        <div className="friends">
+          <div className="pic"></div>
+          <span className="name">Siddha</span>
+        </div>
       </div>
     </div>
   );
--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -21,17 +21,22 @@ function Post() {
       </div>
       <div className="mid">
         <span className="text">Hello World!</span>
-        <picture className="postImage"></picture>
+        <picture className="postImage">
+          <img
+          src='https://images.unsplash.com/photo-1545284234-f4821630060b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHNwbGFzaHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
+            alt=""
+          />
+        </picture>
       </div>
       <div className="bottom">
         <div className="left">
           <ThumbUpAltOutlined className="thumbUp" />
           <FavoriteBorderOutlined className="like" />
           <span className="likeMessage">7 people liked it</span>
-				</div>
-				<div className="right">
-					<span className='comment'>4 comments</span>
-				</div>
+        </div>
+        <div className="right">
+          <span className="comment">4 comments</span>
+        </div>
       </div>
     </div>
   );
